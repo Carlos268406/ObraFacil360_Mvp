075f945163098ca53c0a39cdf201256072316b90
Carlos268406 <carlosantosseduardo@gmail.com>
2026-03-24 02:15:06 -0300

    Estrutura inicial da ObraFácil360 com Flask e Boostrap Funcional

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print ("Sistema obra360 iniciado")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+
+    return render_template('index.html')
+
+if __name__=='__main__':
+
+    app.run(debug=True)
