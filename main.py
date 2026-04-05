64c21496ae09bf0f8c85a18c00997cc98d8e53d1
Carlos268406 <carlosantosseduardo@gmail.com>
2026-04-05 03:41:27 -0300

    Estrutura inicial da ObraFácil360 com Flask e Boostrap Funcional

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from urllib.parse import quote
 
-app = Flask(__name__, template_folder='app/templates')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/solicitar')
-def solicitar():
-    return "<h2>Conte sobre sua obra</h2>"
+@app.route('/enviar', methods=['POST'])
+def enviar():
+    # Agora os nomes dos campos batem com o HTML
+    titulo = request.form.get('nome')
+    bairro = request.form.get('bairro')
 
+    numero_whatsapp = "5511997100740"
+    # Monta a mensagem com os dados recebidos
+    mensagem = f"{titulo} - {bairro}"
+    texto_codificado = quote(mensagem)
 
-if __name__ == "__main__":
+    link_whatsapp = f"https://wa.me/{numero_whatsapp}?text={texto_codificado}"
+    return redirect(link_whatsapp)
+
+if __name__ == '_main_':
     app.run(debug=True)
